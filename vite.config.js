4b7281c73a34d75082fa0174e63c56369e4f5504
PrumSokheng <your-email@example.com>
2025-06-04 21:01:11 +0700

    add  base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/final_project_movie/", // your repo name with slashes
 })
